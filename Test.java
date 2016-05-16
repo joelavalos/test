8414f3b6d569376bf888bf3e737b81f928ceac8d
Joel Test <joel.avalos@usach.cl>
2016-05-15 23:38:41 -0400

    Primer commit

---
+++ Test.java
@@ -0,0 +1,9 @@
+public class Test{
+	public static void main(String[] args) {
+		System.out.println("Hola mundo");
+
+		for (int i = 0; i < 5; i++) {
+			System.out.println("Valor: " + i);
+		}
+	}
+}
